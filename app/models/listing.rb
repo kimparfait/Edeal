22579371b30e6a9b8ea5c2546cba4daf268f5aa6
kim <kimtalya7@gmail.com>
2016-03-08 01:54:58 +0300

    add validation to users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,10 @@ class Listing < ActiveRecord::Base
 	
 	 has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
 	 validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	 
+
+	 validates :name, :description, :price, :address, :phone, presence: true 
+	 validates :price, numericality: { greater_than: 0}
+	 validates :phone, length: { maximum: 14 }
+	 validates :phone, uniqueness: true
+	 validates_attachment_presence :image
 end
