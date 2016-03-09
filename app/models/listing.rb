7839970c9ebaec1aed2476e48f4a9d13c1424023
kim <kimtalya7@gmail.com>
2016-03-09 16:48:28 +0300

    add dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,20 @@
 class Listing < ActiveRecord::Base
 	
-	 has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-	 validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200", :thumb => "100x100>" }, :default_url => "default.jpg"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  else
+    has_attached_file :image, :styles => { :medium => "200", :thumb => "100x100>" }, :default_url => "default.jpg"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/,
+                :storage => :dropbox, :dropbox_credentials => Rails.root.join("config/dropbox.yml"), :path => ":style/:id_filename"
 
-	 validates :name, :description, :price, :address, :phone, presence: true 
-	 validates :price, numericality: { greater_than: 0}
-	 validates :phone, length: { maximum: 14 }
-	 validates :phone, uniqueness: true
-	 validates_attachment_presence :image
-	 belongs_to :user
+end
+
+ validates :name, :description, :price, :address, :phone, presence: true 
+ validates :price, numericality: { greater_than: 0}
+ validates :phone, length: { maximum: 14 }
+
+ validates_attachment_presence :image
+ belongs_to :user
+
 end
